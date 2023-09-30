7fd0a0e89c664be7f1917de55e136c96424f68fc
mr-Bones3108 <nikhil788p@gmail.com>
2023-09-30 18:15:07 +0530

    add a hover effect eventListner in js file

--- app.js
+++ app.js
@@ -6,9 +6,23 @@ function createGrid(rows,cols){
             //create a square element
             const square = document.createElement('div')
             square.classList.add('square')
+            //we can add a button to change color
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = randomColor(); // Change the color to black on hover
+            });
             mainContainer.appendChild(square);
         }
     }
 }
 
+function randomColor(){
+    const r = Math.floor(Math.random()*256)
+    const g = Math.floor(Math.random()*256)
+    const b = Math.floor(Math.random()*256)
+    return `rgb(${r},${g},${b})`;
+}
+console.log(randomColor())
+
 createGrid(16,16)
+
+
